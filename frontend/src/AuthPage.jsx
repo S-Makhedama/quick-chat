d22845cbd5711facc6625099b2f89a1dc2f6eb28
S-Makhedama <samkeliso.makhedama@capaciti.org.za>
2024-04-25 15:31:07 +0200

    update

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,9 +1,18 @@
+//import axios 
+import axios from 'axios'
 
 const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
-      props.onAuth({ username: value, secret: value });
+      //send data to the server
+      axios.post(
+        'http://localhost:3001/authenticate',
+        {username: value}
+      )
+      .then(r => props.onAuth({ ...r.data, secret: value }))
+      .catch(e => console.log('error', e))
+      
     };
   
 
