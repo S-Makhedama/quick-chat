3981d8e270d9b901301977bb89ae63dd874e626a
S-Makhedama <samkeliso.makhedama@capaciti.org.za>
2024-04-26 09:35:28 +0200

    update

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -2,7 +2,8 @@
 import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic('7ab03e68-748d-4929-86be-f3bd8f51c4f8', 
+    const chatProps = useMultiChatLogic(
+    '7ab03e68-748d-4929-86be-f3bd8f51c4f8', 
     props.user.username,
     props.user.secret
 ); 
