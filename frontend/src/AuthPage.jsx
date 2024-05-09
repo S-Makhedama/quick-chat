8545f35b4b3b7d7eb5559238c106cffc734f84bf
Lissa Tshona <62402931+lisatshona@users.noreply.github.com>
2024-05-09 14:50:12 +0200

    Updates

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -20,7 +20,7 @@ const AuthPage = (props) => {
     return (
       <div className="background">
         <form onSubmit={onSubmit} className="form-card">
-          <div className="form-title">Welcome 👋</div>
+          <div className="form-title">Welcome to Quick-Chat 😉</div>
   
           <div className="form-subtitle">Set a username to get started</div>
   
